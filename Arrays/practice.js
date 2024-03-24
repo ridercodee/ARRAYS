052d62c24a0e25cd9e8cd02a7b223d0daa8c2f0b
Mehboob Khan <mehboobrider57@gmail.com>
2024-03-24 17:47:00 +0530

    Update practice.js

--- Arrays/practice.js
+++ Arrays/practice.js
@@ -44,4 +44,47 @@ const comparisonFunction = function(a, b) {
 };
 intArray.sort(comparisonFunction);
 console.log(intArray)
- 
+
+/ Continue and break;
+for (i = 0; i < Array.length; i++) { 
+ if ( arr[i] == undefined ) continue; 
+}
+
+let foundElement;
+for (i = 0; i < Array.length; i++) { 
+  if (array[i].id == idToSearch) { 
+    foundElement = array[i];
+    break;
+  }
+}
+
+// Some and every
+let Ar = [2, 4, 6];
+arr.some(element => { 
+  return element > 4;
+});
+
+// Reduce function
+let discountAmount = cart.product.reduce( 
+  (accumulator, product) => { 
+    return accumulator - product.price;
+  }, 25);
+
+// Include function
+let contentBlocks = [];
+let newBlock = {id: "001", tittle: "Block 1"};
+contentBlocks.push(newBlock);
+
+contentBlocks.includes(newBlock, 3);
+
+// find function 
+let boolmarkedBlocks = 
+course.content.filter( block => { 
+  return block.bookmarked == true;
+})
+
+// Filter function
+let Array1 = [10, 20, 30, 40, 50, 60];
+
+const new1 = Array1.filter((elements) => elements > 10);
+console.log(new1);
